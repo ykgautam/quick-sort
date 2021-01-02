80c70758508220821b77491c19cc407968f3be95
Yash <yk.gautam97@gmail.com>
2021-01-02 15:42:01 +0530

    sortedmap

--- Practices/src/edu/practice/map/SortedMapExample.java
+++ Practices/src/edu/practice/map/SortedMapExample.java
@@ -1,10 +1,37 @@
 package edu.practice.map;
 
+import java.util.Iterator;
+import java.util.Map;
+import java.util.Set;
+import java.util.SortedMap;
+import java.util.TreeMap;
+
 public class SortedMapExample {
-
 	public static void main(String[] args) {
+		
+		SortedMap<Integer, String> sm = new TreeMap<Integer, String>();
+		
+		sm.put(new Integer(2), "java");
+		sm.put(new Integer(3), "python");
+		sm.put(new Integer(5), "javascript");
+		sm.put(new Integer(4), "c");
+		sm.put(new Integer(1), "cpp");
+		
+		System.out.println(sm.containsValue("python"));
+		System.out.println(sm.get(2).equals("java"));
+		
+		Set s = sm.entrySet();
 
-		System.out.println("this is for sorted map examples ");
+		// Using iterator in SortedMap
+		Iterator i = s.iterator();
+
+		while (i.hasNext()) {
+			Map.Entry m = (Map.Entry) i.next();
+
+			int key = (Integer) m.getKey();
+			String value = (String) m.getValue();
+
+			System.out.println("Key : " + key + " value : " + value);
+		}
 	}
-
 }
