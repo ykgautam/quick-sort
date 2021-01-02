74edbe70d72357aadf6259bfe374cf6dd9fd9658
Yash <yk.gautam97@gmail.com>
2021-01-02 15:29:37 +0530

    commit

--- Practices/src/edu/practice/map/SortedMapExample.java
+++ Practices/src/edu/practice/map/SortedMapExample.java
@@ -3,7 +3,7 @@ package edu.practice.map;
 public class SortedMapExample {
 
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
+
 		System.out.println("this is for sorted map examples ");
 	}
 
