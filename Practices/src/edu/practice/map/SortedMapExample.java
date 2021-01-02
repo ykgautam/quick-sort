6bd056ae08f07e20e062f304ff7364c3882fa665
Yash <yk.gautam97@gmail.com>
2021-01-02 15:16:00 +0530

    sortedmap

---
+++ Practices/src/edu/practice/map/SortedMapExample.java
@@ -0,0 +1,10 @@
+package edu.practice.map;
+
+public class SortedMapExample {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		System.out.println("this is for sorted map examples ");
+	}
+
+}
