1179e7d94f42b1111f7d5171980f028ab1c5cc33
Yash <yk.gautam97@gmail.com>
2021-01-07 15:13:52 +0530

    made field as private member

--- java-examples/src/main/java/edu/practice/hashequals/Human.java
+++ java-examples/src/main/java/edu/practice/hashequals/Human.java
@@ -1,40 +1,45 @@
 package edu.practice.hashequals;
+
 public class Human {
-	protected int adharNumber ;
-	
+	private int adharNumber;
+
 	private String name;
-	
+
 	public Human(int adharNumber, String name) {
 		this.adharNumber = adharNumber;
 		this.name = name;
 	}
+
+	public int getAdharNumber() {
+		return adharNumber;
+	}
+
+	public String getName() {
+		return name;
+	}
+
 	@Override
 	public int hashCode() {
 		return adharNumber;
 	}
+
 	@Override
 	public boolean equals(Object obj) {
 		Human h = (Human) obj;
-		if(this == obj) // if yash.equals(yash) 
+		if (this == obj) // if yash.equals(yash)
 		{
-//			System.out.println("fff");
 			return true;
 		}
-//		System.out.println("hh");
-		if(obj == null) // if obj =null  i.e yash.equals(null)
+		if (obj == null) // if obj =null i.e yash.equals(null)
 		{
-//			System.out.println("aaa");
-			return false;		
-		}
-		if(this.adharNumber != h.adharNumber) {
-//			System.out.println("bbb");
 			return false;
 		}
-		if(this.name != h.name) {
-//			System.out.println("ccc");
-			return false;	
+		if (this.adharNumber != h.adharNumber) {
+			return false;
 		}
-//		System.out.println("eee");
+		if (this.name != h.name) {
+			return false;
+		}
 		return true;
 	}
 }
